b6218284d3846361a3810777f5ecb5e59232368e
Luciano Germani <lucianogermani93@gmail.com>
2023-04-09 21:58:41 +0200

    Feat: Settings,Kickoff home/login component

---
+++ src/Containers/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Login                                        from '../Login/Login';
+import                                              './Home.scss';
+
+
+const Home = () => {
+    return(
+        <div className="home-design">
+            Home
+            <Login></Login>
+        </div>
+    )
+}
+
+export default Home;
